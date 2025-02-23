152552b16b4db97ce1b80c969b1ca95e936f139d
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-23 16:27:39 -0500

    Task 3- Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -39,3 +39,23 @@ console.log(mgr1.getDetails());
 
 console.log(mgr1.calculateBonus()); 
 // Expected output: 9600
+
+
+// Task 3 Creating a Company Class 
+class Company {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee) { this.employees.push(employee)}
+    listEmployees() {
+        this.employees.forEach(employee => console.log(employee.getDetails()))
+    }
+} // Creating a company class with property name and an array of employees
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
+// Expected output:
+// "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+// "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
